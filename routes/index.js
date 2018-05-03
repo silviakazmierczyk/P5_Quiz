988b80b83b0f21b7dede20aac7b5555ff2ace84d
silviakazmierczyk <silvia.kazmierczyk@gmail.com>
2018-05-03 21:04:16 +0200

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const controlator = require("../controlador/index");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
+});
 
+router.get('/quizzes', controlator.index);
 module.exports = router;
